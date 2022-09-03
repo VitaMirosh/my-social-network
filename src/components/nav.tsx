08a66fdc75d37a66ed0effd89d8fbba088e849d1
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2022-09-03 21:02:37 +0300

    first commit

---
+++ src/components/nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src = "https://bipbap.ru/wp-content/uploads/2017/04/priroda_kartinki_foto_03.jpg"></img>
+    </header>
+    ) }
+export default Header;
